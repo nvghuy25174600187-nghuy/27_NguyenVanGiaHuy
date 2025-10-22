7e56d6f12982d712209496c657684fb69b2d8ce7
01_Le_Van_An <email@sv.uneti.edu.vn>
2025-10-22 11:38:32 +0700

    Hoan thanh Bai2

---
+++ Chuong3/Bai2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Nhap so nguyen n: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Vui long nhap so nguyen duong lon hon 0");
+    } else {
+        printf("Day so tu 1 den %d la:\n", n);
+        for (int i = 1; i <= n; i++) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
